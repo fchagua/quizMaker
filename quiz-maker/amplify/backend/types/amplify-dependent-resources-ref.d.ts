60beecd4e64cb60e9d38befbf4fec0002550ed62
Francisco Chagua <fchagua@purdue.edu>
2024-12-04 13:33:57 -0600

    Initial quiz maker react web app

---
+++ quiz-maker/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quizmaker99efbc36": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
